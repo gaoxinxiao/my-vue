cacbb95fc780677d13b3dc9adb8984d6d2a835eb
gaoxinxiao <18210833386@163.com>
2020-12-27 15:58:03 +0800

    备份

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,16 +1,81 @@
 <template>
-  <div id="app">
-    
+  <div id="kline" ref="kline" style="height: 100%">
+    <div class="header">
+      <span @click="showList" >123</span>
+      {{ name }}
+    </div>
+    <div class="main" ref="main"></div>
+    <List
+      :show="listshow"
+      @toggleShow="showList"
+      @ChangeSymbol="ChangeSymbol"
+      :pairName="pairName"
+    />
   </div>
 </template>
 
 <script>
-
+import List from "./list.vue";
 export default {
-  name: 'Home',
-}
+  data() {
+    return {
+      listshow: false,
+    };
+  },
+
+  components: {
+    List,
+  },
+
+  methods: {
+    showList() {
+      if (this.listshow) {
+        setTimeout(() => {
+          this.listshow = false;
+        }, 500);
+      } else {
+        this.listshow = true;
+      }
+    },
+  },
+};
 </script>
-
-<style>
-
+<style scoped>
+::-webkit-scrollbar {
+  display: none;
+}
+#kline {
+  width: 100%;
+  height: 100%;
+  background: #071724;
+  overflow: hidden;
+  padding-top: 10%;
+  box-sizing: border-box;
+}
+.header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 5%;
+  color: #f7f7f7;
+  background: #131e30;
+  z-index: 1001;
+  display: flex;
+  align-items: center;
+  padding: 0 4%;
+  > img:nth-of-type(1) {
+    height: 14px;
+  }
+  > img:nth-last-of-type(1) {
+    height: 14px;
+    width: 18px;
+    margin: 0 8px;
+  }
+}
+.main {
+  height: 100%;
+  width: 100%;
+  overflow-y: auto;
+}
 </style>
--- src/views/list.vue
+++ src/views/list.vue
@@ -1,16 +1,68 @@
 <template>
-  <div id="app">
-   list
+  <div
+    class="coinlist"
+    v-if="show"
+    @click.self="hideList"
+    ref="list"
+    :style="{ background: bgColor }"
+  >
+    <div :class="animationList"></div>
+    Sdadasd
   </div>
 </template>
-
 <script>
-
 export default {
-  name: 'List',
-}
+  props: ["show"],
+  data() {
+    return {
+      animationList: "list show",
+      bgColor: "rgba(0,0,0,.5)",
+    };
+  },
+  methods: {
+    hideList() {
+      this.animationList = "list hide";
+      this.bgColor = "transparent"; // 遮罩层的背景色在显示动画中有颜色，消失时为透明
+      this.$emit("toggleShow"); // 调用父组件的隐藏方法
+      setTimeout(() => {
+        // 设置延时是因为消失后需要重新定义方法，便于下一次出现时动画
+        this.animationList = "list show";
+        this.bgColor = "rgba(0,0,0,.5)";
+      }, 501);
+    },
+  },
+};
 </script>
-
-<style>
-
+<style scoped>
+.coinlist {
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 1002;
+}
+.show {
+  animation: leftShow 0.5s;
+}
+.hide {
+  animation: leftHide 0.5s;
+}
+@keyframes leftShow {
+  0% {
+    transform: translate(-100%, 0);
+  }
+  100% {
+    transform: translate(0, 0);
+  }
+}
+@keyframes leftHide {
+  0% {
+    transform: translate(0, 0);
+  }
+  100% {
+    transform: translate(-100%, 0);
+  }
+}
 </style>
